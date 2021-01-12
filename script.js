6ee4470b339eb3ca4740b912fc73f0aa8dd93b54
pern7295 <pern7295@stud.kea.dk>
2021-01-12 17:52:22 +0100

    firstcommit

---
+++ script.js
@@ -0,0 +1,20 @@
+/*---BURGERMENU----*/
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+    document.querySelector("#menu").addEventListener("click", onClickMenu);
+
+}
+
+/*--FUNCTION TIL BURGERMENU---*/
+function onClickMenu() {
+    console.log("onClickMenu");
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu_img").classList.toggle("change_img");
+
+    document.getElementById("burger1").classList.toggle("change1");
+    document.getElementById("burger2").classList.toggle("change1");
+    document.getElementById("burger3").classList.toggle("change1");
+}
